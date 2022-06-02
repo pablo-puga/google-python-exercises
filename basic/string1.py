aed0e6944d49c233ec3d403356efac33e660ad51
Pablo Puga <pablo.puga.git@gmail.com>
2022-06-02 17:41:52 +0200

    Implemented Basic exercise string1.py

--- basic/string1.py
+++ basic/string1.py
@@ -25,7 +25,9 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  if count >= 10:
+    return 'Number of donuts: many'
+  return 'Number of donuts: %d' % count
 
 
 # B. both_ends
@@ -35,7 +37,9 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  if len(s) < 2:
+    return ''
+  return s[:2] + s[-2:]
 
 
 # C. fix_start
@@ -49,7 +53,8 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  tmp = s.replace(s[0], '*')
+  return s[0] + tmp[1:]
 
 
 # D. MixUp
@@ -61,7 +66,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
