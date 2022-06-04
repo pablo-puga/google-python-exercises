7a45146d93dcaf0ea636565f354c0043a4e76a56
Pablo Puga <pablo.puga.git@gmail.com>
2022-06-04 13:39:58 +0200

    Implemented Basic exercise wordcount.py

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -37,6 +37,7 @@ print_words() and print_top().
 
 """
 
+from pprint import pprint
 import sys
 
 # +++your code here+++
@@ -46,12 +47,47 @@ import sys
 # Then print_words() and print_top() can just call the utility function.
 
 ###
+def read_file(filename):
+  content = ''
+  with open(filename, 'rU') as file:
+    content = file.read()
+  return content
+
+def get_words(string):
+  return [w.lower() for w in string.split()]
+
+def build_word_dict(words):
+  word_dic = {}
+
+  for word in words:
+    word_dic[word] = word_dic.get(word, 0) + 1
+
+  return word_dic
+
+def build_word_dict_from_file(filename):
+  return build_word_dict(get_words(read_file(filename)))
+
+def print_words(filename):
+  words_dict = build_word_dict_from_file(filename)
+
+  words = sorted(words_dict.keys())
+  for word in words:
+    print ('%s %d' % (word, words_dict[word]))
+
+def print_top(filename):
+  words_dict = build_word_dict_from_file(filename)
+
+  words_data = sorted(words_dict.items(), key=lambda x: x[1], reverse=True)
+
+  for word_data in words_data[:20]:
+    print('%s %d' % word_data)
+
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print ('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -61,7 +97,7 @@ def main():
   elif option == '--topcount':
     print_top(filename)
   else:
-    print 'unknown option: ' + option
+    print ('unknown option: ' + option)
     sys.exit(1)
 
 if __name__ == '__main__':
